3a32b4dca1e0563d8b1d59c91e77aa5e1944c1d6
valbuilds <katie@katiebuilds.website>
2023-10-13 15:57:05 -0500

    update

--- commands/incompetency.py
+++ commands/incompetency.py
@@ -1,6 +1,20 @@
 import discord
 from discord.ext import commands
+from discord import app_commands
 from typing import Optional
+
+class IncompetencyA(app_commands.Group):
+    @app_commands.command(description="Report a blooper.")
+    async def blooper(self, interaction: discord.Interaction, description: str, attachment: Optional[discord.Attachment]):
+        c = interaction.client.get_guild(1016626731785928715).get_channel(1016692053280817172)
+        e = discord.Embed(title="Blooper reported!", description=f"Reported by: {interaction.user.mention}", colour=discord.Colour.brand_red())
+        e.add_field(name="Description of blooper", value=description)
+        if attachment is not None:
+            if attachment.content_type.startswith('image') or attachment.content_type.startswith('video'):
+                e.set_image(url=attachment.proxy_url)
+        cm = await c.send(embed=e)
+        await cm.create_thread(name=f"Thread for {interaction.user.display_name}", reason="Blooper reported")
+        await interaction.response.send_message(content=cm.jump_url, ephemeral=True)
 
 class Incompetency(commands.Cog):
     def __init__(self, bot):
@@ -12,10 +26,14 @@ class Incompetency(commands.Cog):
         c = ctx.bot.get_guild(1016626731785928715).get_channel(1016692053280817172)
         e = discord.Embed(title="Blooper reported!", description=f"Reported by: {ctx.author.mention}", colour=discord.Colour.brand_red())
         e.add_field(name="Description of blooper", value=d)
+        if ctx.message.attachments is not None:
+            if ctx.message.attachments[0].content_type.startswith('image') or ctx.message.attachments[0].content_type.startswith('video'):
+                e.set_image(url=ctx.message.attachments[0].proxy_url)
         cm = await c.send(embed=e)
         await cm.create_thread(name=f"Thread for {ctx.author.display_name}", reason="Blooper reported")
         await ctx.reply(content=cm.jump_url)
 
 async def setup(bot):
     await bot.add_cog(Incompetency(bot))
+    bot.tree.add_command(IncompetencyA(name="incompetency", description="Incompetency commands"))
     print("commands.incompetency ready :)")
--- main.py
+++ main.py
@@ -17,26 +17,34 @@ def run():
     async def on_message(message: discord.Message):
         if message.author.bot == False and bot.user.mentioned_in(message):
             await message.channel.send("<a:pepeWut:1110185838463160420>")
+        elif message.author.bot == False and '<:IDIOT:1109489652555063296>' in message.clean_content or message.clean_content == '<:IDIOT:1109489652555063296>':
+            await message.add_reaction('<:IDIOT:1109489652555063296>')
+        elif message.author.bot == True and message.channel == bot.get_guild(1016626731785928715).get_channel(1058443175427047555) and message.embeds is not None:
+            await message.create_thread(name=message.embeds[0].description[:100], reason="Suggestion")
         else:
             await bot.process_commands(message)
     
-    @bot.command()
+    @bot.command(brief="Aaron Hesselhusrt, my beloved.", description="Sends an image of Aaron Hesselhurst, one of the best BBC News presenters.")
     async def aaron(ctx: commands.Context):
         await ctx.reply(content="[.](https://cdn.discordapp.com/attachments/1097533655682912416/1162441481148170252/aaron.png?ex=653bf2fe&is=65297dfe&hm=8f17e3c5a6243c508caa8bf0907f359fd84b09fd2ee1f6782e749960929cce69&)")
 
-    @bot.command()
+    @bot.command(brief="DO YOU WANT SOME FACTS ON BAGUETTES???", description="Sends a video of Christian Frazier giving some facts on baguettes.")
     async def baguette(ctx: commands.Context):
         await ctx.reply(content="[.](https://cdn.discordapp.com/attachments/1016691910158590032/1162430490981171270/baguette.mov?ex=653be8c1&is=652973c1&hm=db630770104c537f21ff2e3609ba2515c8ebfa9717615855beea973becc71be5&)")
 
-    @bot.command()
+    @bot.command(brief=">:(", description="Tries to send the source code for Bot Bot Command News, but egg won't share. >:(")
     async def botbotsource(ctx: commands.Context):
         return await ctx.reply(content="egg wont share <:broWhat:1100474984738070639>")
     
-    @bot.command()
+    @bot.command(brief=":)", description="Sends a link to my source code! ^-^")
+    async def source(ctx: commands.Context):
+        return await ctx.reply(content="https://github.com/valbuilds/bee-bee-see-bot")
+    
+    @bot.command(brief="minty", description="mint")
     async def mint(ctx: commands.Context):
         return await ctx.reply(content="<:greenfrog:1115049036735594506>")
     
-    @bot.command()
+    @bot.command(brief="The bank was well prepared.", description="Sends an image of Sophie Raworth, who's told us that the bank was well prepared.")
     async def sophie(ctx: commands.Context):
         return await ctx.reply(content="[.](https://cdn.discordapp.com/attachments/1162403788175454251/1162460726951170078/p0fh3vw0.jpg?ex=653c04ea&is=65298fea&hm=62866c61f2fb5ae388314dff55badc28a43bb1aebf834858cfcb0136f1c2e093&)")
 
@@ -55,7 +63,13 @@ def run():
 
         await interaction.response.send_modal(TestModal())
 
-    @bot.command()
+    # @bot.tree.command(name="normal")
+    # async def normal(interaction: discord.Interaction, message: discord.Message):
+    #     await message.reply(content="normal", mention_author=False)
+    #     return await interaction.response.send_message(content="normal", ephemeral=True)
+
+    @bot.command(brief="Syncs application commands with Discord.", description="(OWNER ONLY) Syncs application commands with Discord.")
+    @commands.is_owner()
     async def sync(ctx: commands.Context):
         await bot.tree.sync()
         await ctx.reply("Synced.")
