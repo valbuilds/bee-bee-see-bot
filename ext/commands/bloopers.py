004b92ec60e1f7e18e2a44436ad34e46c30cded0
valbuilds <katie@katiebuilds.website>
2023-10-19 22:51:50 -0500

    a few changes

--- ext/commands/bloopers.py
+++ ext/commands/bloopers.py
@@ -1,3 +1,4 @@
+# import modules
 import discord
 from discord.ext import commands
 import random
@@ -5,6 +6,13 @@ import datetime
 import time
 import pytz
 
+# import other files
+import settings
+
+# logger
+logger = settings.logging.getLogger("bot")
+
+# check time
 async def check_time(time: str):
     if time[2] != ":":
         return False
@@ -14,6 +22,7 @@ async def check_time(time: str):
         else:
             return False
 
+# turn text into timestamp
 def texttime_to_timestamp(timee: str, type: str):
     allowed_types = ["d", "D", "t", "T", "f", "F", "R", "."]
     if type not in allowed_types:
@@ -27,15 +36,18 @@ def texttime_to_timestamp(timee: str, type: str):
         else:
             return "<t:" + str(int(time.mktime(timea.timetuple()))) + ":" + type + ">"
 
+# cog
 class Bloopers(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
+    # command group
     @commands.group(breif="Blooper commands!")
     async def blooper(self, ctx: commands.Context):
         if ctx.subcommand_passed is None:
             return ctx.reply(content="{ctx.subcommand_passed} is not part of the bloopers group.", delete_after=60, mention_author=False)
 
+    # blooper report command
     @blooper.command(breif="Reports a blooper!", description="Reports a blooper to the current channel!")
     async def report(self, ctx: commands.Context, time: str, *description: str):
         id = random.randrange(1001, 10001)
@@ -53,3 +65,8 @@ class Bloopers(commands.Cog):
         embed.add_field(name="Description", value=" ".join(description))
         
         return await ctx.reply(embed=embed, mention_author=False)
+    
+# add cog to bot
+async def setup(bot: commands.Bot):
+    await bot.add_cog(Bloopers(bot))
+    logger.info("ext.events.suggestions okay")
