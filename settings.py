a860ee904c84a43cdf1bfe8333e86d38ecaa8846
valbuilds <katie@katiebuilds.website>
2023-10-19 21:35:49 -0500

    new settings

--- settings.py
+++ settings.py
@@ -1,18 +1,69 @@
+# import modules
 import os
-import json
 from dotenv import load_dotenv
 load_dotenv()
+import json
 
-TOKEN = os.getenv('token')
+# import other files
+import logging
+from logging.config import dictConfig
 
+# make config accessible
+cfile = open('config.json')
+cdata =  json.load(cfile)
+
+# access config using this
 class Config:
-    file = open('config/main.json')
-    data = json.load(file)
+    # enable debug version
+    debug: bool = True
+    # debug-specific config
+    class Debug:
+        token: str = os.getenv('debugtoken')
+        guildid: int = cdata['debug']['guildid']
+        suggestionchannelid: int = cdata['debug']['suggestionchannelid']
+    # main-specific config
+    class Main:
+        token: str = os.getenv('maintoken')
+        guildid: int = cdata['main']['guildid']
+        suggestionchannelid: int = cdata['main']['suggestionchannelid']
 
-    prefix = data['prefix']
+# logging stuff
+loggingconfig = {
+    "version": 1,
+    "disabled_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)-10s - %(asctime)s - %(module)-15s : %(message)s"
+        },
+        "standard": {"format": "%(levelname)-10s - %(name)-15s : %(message)s"},
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "standard",
+        },
+        "console2": {
+            "level": "WARNING",
+            "class": "logging.StreamHandler",
+            "formatter": "standard",
+        },
+        "file": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": "logs/infos.log",
+            "mode": "w",
+            "formatter": "verbose",
+        },
+    },
+    "loggers": {
+        "bot": {"handlers": ["console"], "level": "INFO", "propagate": False},
+        "discord": {
+            "handlers": ["console2", "file"],
+            "level": "INFO",
+            "propagate": False,
+        },
+    },
+}
 
-    main_guild_id = data['main_guild_id']
-    bloopers_channel = data['bloopers_channel']
-    suggestions_channel = data['suggestions_channel']
-    home_guild_id = data['home_guild_id']
-    home_suggestions_channel = data['home_suggestions_channel']
+dictConfig(loggingconfig)
